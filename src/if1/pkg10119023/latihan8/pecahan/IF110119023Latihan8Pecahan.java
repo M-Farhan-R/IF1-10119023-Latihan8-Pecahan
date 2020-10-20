98fa4512a96b8fbffab32ffcacab70ad5743c183
Farhan <farhan.10119023@mahasiswa.unikom.ac.id>
2020-10-20 20:03:16 +0700

    Latihan 8 - Pecahan

---
+++ src/if1/pkg10119023/latihan8/pecahan/IF110119023Latihan8Pecahan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119023.latihan8.pecahan;
+
+/**
+ *
+ * @author    
+ * NAMA     : Muhammad Farhan R
+ * KELAS    : IF1
+ * NIM      : 10119023
+ * Deskripsi Program : latihan membuat bariabel bertipe data real
+ */
+public class IF110119023Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789f;
+        d = 1_234_567_890.123_456_789;
+        System.out.println("Float : "+ f);
+        System.out.println("Duble : "+ d);
+    }
+    
+}
